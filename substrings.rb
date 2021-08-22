f490a6d77c89e638188dbc9c36ae536d1b7a5d77
idinyte <black.mad.eagle@gmail.com>
2021-08-22 12:33:11 +0300

    initial build

---
+++ substrings.rb
@@ -0,0 +1,12 @@
+def substrings(text, dict)
+    matches = {}
+    dict.each do |word|
+        match = text.downcase.scan(word)
+        matches[match[0]] = match.length
+    end
+    matches.delete(nil)
+    matches
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
